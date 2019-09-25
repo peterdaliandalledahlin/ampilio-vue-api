d5cbfa2c0efaf4dbf76921c2a1c5c0a7c84c1684
Peter Dahlin <peterdaliandalledahlin@gmail.com>
2019-09-25 11:13:31 +0200

    fixed nav link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#"><img src="../assets/images/site-logo.png" alt="ampilio"></a>
+        <router-link class="navbar-brand" to="/"><img src="../assets/images/site-logo.png" alt="ampilio"></router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
